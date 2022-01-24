c943a9708ca38109496d13012b68f6958e6bbc65
dev <dev@rabins-iMac.local>
2022-01-23 22:27:34 -0800

    operator created

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // int, float, char, String, long, double, boolean
+
+//        int marks = 18;
+//        float averageMarks = 19.345f;
+//        char grade = 'A';
+//        String name = "Rabin";
+//        boolean isValid = false;
+//
+//        long bigNumber = 9876543l;
+
+//        System.out.println(marks);
+
+        // +, -, /, *, %, ++, --
+
+        int firstNumber = 123;
+        int secondNumber = 234;
+
+        int sum = firstNumber + secondNumber;
+        System.out.println(sum);
+
+    }
+}
