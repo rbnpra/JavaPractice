248f06c722015ad5dd56ba3cfda375e74c2f3580
dev <dev@rabins-iMac.local>
2022-01-23 22:38:13 -0800

    created increment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,11 +18,23 @@ public class Main {
 
         // +, -, /, *, %, ++, --
 
-        int firstNumber = 123;
-        int secondNumber = 234;
+//        int firstNumber = 123;
+//        int secondNumber = 234;
+//
+//        int sum = firstNumber + secondNumber;
+//        System.out.println(sum);
+//        int firstNumber = 1234;
+//        int secondNumber = 234;
+//
+//        double sum = (double)firstNumber % secondNumber;
+//        System.out.println(sum);
 
-        int sum = firstNumber + secondNumber;
-        System.out.println(sum);
-
+    int a = 12;
+//    a = a + 1; this statement increments by 1.
+    a++;
+        System.out.println(a++);
+        System.out.println(a);
+    a--;
+        System.out.println(a);
     }
 }
